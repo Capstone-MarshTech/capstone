b56b79f1f0e9e04e22bc3bef59e36d927c6d63b1
Ethan Lee <ethanlee.dev1@gmail.com>
2023-08-25 16:45:14 -0400

    Finish setting backend folder structure with .gitignore

---
+++ Server/scripts/script.py
@@ -0,0 +1,19 @@
+#  read original csv
+
+# only keep these columns:
+# claim_number
+# effective_date
+# cleansed_policyyear
+# total_net_paid
+# total_net_os
+# total_net_incurred
+# open_claim
+# closed_claim
+# zero_value_claim
+# loss_banding
+# marsh_line_of_business_1
+# marsh_line_of_business_2
+
+# store the new csv file to mongodb_uploads
+
+# feed the mongodb with clean csv
