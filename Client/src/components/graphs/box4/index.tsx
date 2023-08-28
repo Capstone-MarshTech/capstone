bae391e7fc4ffc7496ee258071a6ccfc2a0b423e
Ethan Lee <ethanlee.dev1@gmail.com>
2023-08-28 17:38:55 -0400

    graph4 has data

--- Client/src/components/graphs/box4/index.tsx
+++ Client/src/components/graphs/box4/index.tsx
@@ -1,5 +1,9 @@
 import DashboardBox from "@/components/DashboardBox";
-import { useGetLossBandingQuery, useGetYearsQuery } from "@/state/api";
+import {
+  useGetAverageTotalIncurredByLossBandingQuery,
+  useGetLargestClaimsByLossBandingQuery,
+  useGetLossBandingQuery,
+} from "@/state/api";
 
 import {
   ComposedChart,
@@ -13,57 +17,38 @@ import {
   Line,
 } from "recharts";
 
-//   const data = [
-// 	{
-// 	  name: "Page A",
-// 	  uv: 4000,
-// 	  pv: 2400,
-// 	  amt: 2400
-// 	},
-// 	{
-// 	  name: "Page B",
-// 	  uv: 3000,
-// 	  pv: 1398,
-// 	  amt: 2210
-// 	},
-// 	{
-// 	  name: "Page C",
-// 	  uv: 2000,
-// 	  pv: 9800,
-// 	  amt: 2290
-// 	},
-// 	{
-// 	  name: "Page D",
-// 	  uv: 2780,
-// 	  pv: 3908,
-// 	  amt: 2000
-// 	},
-// 	{
-// 	  name: "Page E",
-// 	  uv: 1890,
-// 	  pv: 4800,
-// 	  amt: 2181
-// 	},
-// 	{
-// 	  name: "Page F",
-// 	  uv: 2390,
-// 	  pv: 3800,
-// 	  amt: 2500
-// 	},
-// 	{
-// 	  name: "Page G",
-// 	  uv: 3490,
-// 	  pv: 4300,
-// 	  amt: 2100
-// 	}
-//   ];
-
 type Props = {};
 
 function GraphsBox4({}: Props) {
   const { data } = useGetLossBandingQuery();
-  console.log(data);
 
+  const largestClaims = data
+    ? data.map((eachBanding) => ({
+        lossBanding: eachBanding,
+        data: useGetLargestClaimsByLossBandingQuery({
+          lossBanding: eachBanding,
+        }).data,
+      }))
+    : [];
+
+  const averageTotalIncurred = data
+    ? data.map((eachBanding) => ({
+        lossBanding: eachBanding,
+        data: useGetAverageTotalIncurredByLossBandingQuery({
+          lossBanding: eachBanding,
+        }).data,
+      }))
+    : [];
+
+  const newData = data
+    ? data.map((eachBanding, index) => ({
+        "Loss Banding": eachBanding,
+        "Average Total Incurred": largestClaims[index]?.data,
+        "Largest Claim": averageTotalIncurred[index]?.data,
+      }))
+    : [];
+
+  console.log(newData);
   return (
     <>
       <DashboardBox bgcolor="#fff" gridArea="b4">
@@ -72,7 +57,7 @@ function GraphsBox4({}: Props) {
           <ComposedChart
             width={200}
             height={400}
-            data={data}
+            data={newData}
             margin={{
               top: 20,
               right: 20,
@@ -81,13 +66,18 @@ function GraphsBox4({}: Props) {
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
+            <XAxis dataKey="Loss Banding" />
+            <YAxis label={"Largest Claim"} />
+
             <Tooltip />
             <Legend />
-            <Bar dataKey="pv" stackId="a" fill="#002c77" />
+            <Bar dataKey="Largest Claim" stackId="a" fill="#002c77" />
             {/* <Bar dataKey="amt" stackId="a" fill="#76d3ff" /> */}
-            <Line type="monotone" dataKey="uv" stroke="#76d3ff" />
+            <Line
+              type="monotone"
+              dataKey="Average Total Incurred"
+              stroke="#76d3ff"
+            />
           </ComposedChart>
         </ResponsiveContainer>
       </DashboardBox>
