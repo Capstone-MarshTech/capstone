3b3e73c5bf367ec19209416f5c0449131e398ba8
MichaelAnthonyReidJr <EfficiencyMarriesEfficacy@gmail.com>
2023-08-29 15:10:52 -0400

    Change endpoints.txt, updatae models, dropdown ordering

--- Server/controllers/claimsController.js
+++ Server/controllers/claimsController.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
+import Claim from '../models/ClaimModel.js';
 
 export const closedCount = async (req, res) => {
     const  year  = parseInt(req.params.year)
 
     try { 
-        const db = mongoose.connection.db
-        const claims_count = await db.collection('claim').countDocuments({ closed_claim: true, cleansed_policyyear: year }); 
+        
+        const claims_count = await Claim.countDocuments({ closed_claim: true, cleansed_policyyear: year }); 
         res.json(claims_count);
     } catch (error) {
         res.status(error.statusCode || 500).json({ message: error.message });
@@ -16,8 +17,8 @@ export const openCount = async (req, res) => {
     const  year  = parseInt(req.params.year)
 
     try { 
-        const db = mongoose.connection.db
-        const claims_count = await db.collection('claim').countDocuments({ open_claim: true, cleansed_policyyear: year }); 
+        
+        const claims_count = await Claim.countDocuments({ open_claim: true, cleansed_policyyear: year }); 
         res.json(claims_count);
     } catch (error) {
         res.status(error.statusCode || 500).json({ message: error.message });
@@ -28,8 +29,8 @@ export const zeroValueCount = async (req, res) => {
     const  year  = parseInt(req.params.year)
 
     try { 
-        const db = mongoose.connection.db
-        const claims = await db.collection('claim').countDocuments({ zero_value_claim: true, cleansed_policyyear: year }) 
+        
+        const claims = await Claim.countDocuments({ zero_value_claim: true, cleansed_policyyear: year }) 
         res.json(claims);
     } catch (error) {
         res.status(error.statusCode || 500).json({ message: error.message });
@@ -40,8 +41,8 @@ export const totalNetPaid = async (req, res) => {
     const  year  = parseInt(req.params.year)
 
     try { 
-        const db = mongoose.connection.db
-        const total_net_paid_keyValue = await db.collection('claim').aggregate([
+        
+        const total_net_paid_keyValue = await Claim.aggregate([
             {
                 $match: { cleansed_policyyear: year }
             },
@@ -56,7 +57,8 @@ export const totalNetPaid = async (req, res) => {
                     total_net_paid: 1
                 }
             }
-        ]).toArray();
+
+        ])
         const total_net_paid_value = total_net_paid_keyValue[0].total_net_paid;
         res.json(total_net_paid_value);
     }catch (error){
@@ -69,8 +71,8 @@ export const largest = async (req, res) => {
     const  year  = parseInt(req.params.year)
 
     try { 
-        const db = mongoose.connection.db
-        const max_claim_keyValue = await db.collection('claim').aggregate([
+        
+        const max_claim_keyValue = await Claim.aggregate([
             {
                 $match: { cleansed_policyyear: year }
             },
@@ -85,7 +87,7 @@ export const largest = async (req, res) => {
                     max_paid: 1
                 }
             }
-        ]).toArray();
+        ]);
         const max_claim_value = max_claim_keyValue[0].max_paid;
         res.json(max_claim_value);
     }catch (error){
@@ -98,8 +100,8 @@ export const totalOutstanding = async (req, res) => {
 const  year  = parseInt(req.params.year)
 
     try { 
-        const db = mongoose.connection.db
-        const total_outstanding_keyValue = await db.collection('claim').aggregate([
+        
+        const total_outstanding_keyValue = await Claim.aggregate([
             {
                 $match: { cleansed_policyyear: year }
             },
@@ -114,7 +116,7 @@ const  year  = parseInt(req.params.year)
                     total_outstanding: 1
                 }
             }
-        ]).toArray();
+        ]);
         const total_outstanding_value = total_outstanding_keyValue[0].total_outstanding;
         res.json(total_outstanding_value);
     }catch (error){
--- Server/scripts/clean_csv.py
+++ Server/scripts/clean_csv.py
@@ -30,14 +30,13 @@ columns_to_keep = ["claim_number",
 selected_data = data[columns_to_keep]
 
 # convert types
-# selected_data.loc[:, "effective_date"] = pd.to_datetime(selected_data["effective_date"])
+selected_data.loc[:, "effective_date"] = pd.to_datetime(selected_data["effective_date"])
 selected_data.loc[:, "open_claim"] = selected_data["open_claim"].astype(bool)
 selected_data.loc[:, "closed_claim"] = selected_data["closed_claim"].astype(bool)
 selected_data.loc[:, "zero_value_claim"] = selected_data["zero_value_claim"].astype(bool)
-# selected_data.loc[:, "total_net_paid"] = selected_data["total_net_paid"].astype(float).round(2)
-# selected_data.loc[:, "total_net_incurred"] = selected_data["total_net_incurred"].astype(float).round(2)
-# selected_data.loc[:, "total_net_os"] = selected_data["total_net_os"].astype(float).round(2)
-
+selected_data.loc[:, "total_net_paid"] = selected_data["total_net_paid"].astype(float).apply(lambda x: f"{x:.2f}")
+selected_data.loc[:, "total_net_incurred"] = selected_data["total_net_incurred"].astype(float).apply(lambda x: f"{x:.2f}")
+selected_data.loc[:, "total_net_os"] = selected_data["total_net_os"].astype(float).apply(lambda x: f"{x:.2f}")
 
 # drop duplicate rows
 selected_data = selected_data.drop_duplicates()
