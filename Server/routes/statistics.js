f108940ee3d2efb9ace7a7317dae36165f741e70
Michael Reid <EfficiencyMarriesEfficacy@gmail.com>
2023-08-30 16:47:17 -0400

    add four endpoints,  edit controllers and routes, update enpoints.txt

--- Server/routes/statistics.js
+++ Server/routes/statistics.js
@@ -1,27 +1,31 @@
 import express from 'express';
 import {
   totalIncurredByLossBanding,
-  distinctClaimNumbersByLossBanding,
+  ClaimNumbersByLossBandingCount,
   largestClaimByLossBanding,
   averageTotalIncurredByLossBanding,
   
   totalIncurredByPolicyYearByLossBanding,
-  distinctClaimNumbersByPolicyYearByLossBanding,
+  ClaimNumbersByPolicyYearByLossBandingCount,
   averageTotalIncurredByPolicyYearbyLossBanding,
   largestClaimByPolicyYearByLossBanding,
+  lossBandingValuesByYear,
 } from '../controllers/statisticsController.js';
 
 const router = express.Router();
 // All Years
 router.get('/total_incurred_by', totalIncurredByLossBanding);
-router.get('/distinct_claim_numbers_by', distinctClaimNumbersByLossBanding);
+router.get('/number_of_claims_by', ClaimNumbersByLossBandingCount);
 router.get('/largest_claim_by', largestClaimByLossBanding);
 router.get('/average_total_incurred_by', averageTotalIncurredByLossBanding);
 
 // By Year
 router.get('/total_incurred_by/:year', totalIncurredByPolicyYearByLossBanding);
-router.get('/distinct_claim_numbers_by/:year', distinctClaimNumbersByPolicyYearByLossBanding);
+router.get('/number_of_claims_by/:year', ClaimNumbersByPolicyYearByLossBandingCount);
 router.get('/average_total_incurred_by/:year', averageTotalIncurredByPolicyYearbyLossBanding)
 router.get('/largest_claim_by/:year', largestClaimByPolicyYearByLossBanding);
+router.get('/loss_banding_values_by/:year', lossBandingValuesByYear)
+
 export default router;
+
 
