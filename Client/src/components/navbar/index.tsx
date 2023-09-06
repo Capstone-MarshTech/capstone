b206d8173507ba40d8085da16c5c50c6b6957240
xani-dev <xani.gn@gmail.com>
2023-09-06 10:18:28 -0400

    Adding pointer to all menu items

--- Client/src/components/navbar/index.tsx
+++ Client/src/components/navbar/index.tsx
@@ -25,63 +25,65 @@ const NavBar = ({ captureScreenshot }) => {
   };
 
   return (
-    <FlexBetween
-      mt=".25rem"
-      gap="2rem"
-      p=".3rem 0rem"
-      color={palette.primary[500]}
-    >
-      {/* Download */}
-      <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-        <FileDownloadIcon
-          sx={{ fontSize: "24px", verticalAlign: "text-bottom" }}
-          onClick={() => captureScreenshot()}
-        />
-        <span> Download</span>
-      </Box>
+		<FlexBetween
+			mt='.25rem'
+			gap='2rem'
+			p='.3rem 0rem'
+			color={palette.primary[500]}
+		>
+			{/* Download */}
+			<Box
+				sx={{ '&:hover': { color: palette.primary[100] }, cursor: 'pointer' }}
+			>
+				<FileDownloadIcon
+					sx={{ fontSize: '24px', verticalAlign: 'text-bottom' }}
+					onClick={() => captureScreenshot()}
+				/>
+				<span> Download</span>
+			</Box>
 
-      {/* Filter */}
-      <Box
-        sx={{ "&:hover": { color: palette.primary[100] } }}
-        onClick={handleFilterClick}
-      >
-        <TuneIcon sx={{ fontSize: "24px", verticalAlign: "text-bottom" }} />
-        <span> Filters</span>
-      </Box>
+			{/* Filter */}
+			<Box
+				sx={{ '&:hover': { color: palette.primary[100] }, cursor: 'pointer' }}
+				onClick={handleFilterClick}
+			>
+				<TuneIcon sx={{ fontSize: '24px', verticalAlign: 'text-bottom' }} />
+				<span> Filters</span>
+			</Box>
 
-      {/* BarChart View */}
-      <Box>
-        <Link to="/">
-          <BarChartIcon
-            sx={{
-              fontSize: "24px",
-              color: palette.primary[500],
-              verticalAlign: "text-bottom",
-              "&:hover": { color: palette.primary[100] },
-            }}
-          />
-        </Link>
-      </Box>
+			{/* BarChart View */}
+			<Box>
+				<Link to='/'>
+					<BarChartIcon
+						sx={{
+							fontSize: '24px',
+							color: palette.primary[500],
+							verticalAlign: 'text-bottom',
+							'&:hover': { color: palette.primary[100] },
+						}}
+					/>
+				</Link>
+			</Box>
 
-      {/* Table View */}
-      <Box>
-        <Link to="/table-view">
-          <TableChartIcon
-            sx={{
-              fontSize: "24px",
-              color: palette.primary[500],
-              verticalAlign: "text-bottom",
-              "&:hover": { color: palette.primary[100] },
-            }}
-          />
-        </Link>
-      </Box>
-      {/* Render the Filter component as a pop-up */}
-      {isFilterOpen && (
-        <Filter isOpen={isFilterOpen} onClose={handleCloseFilter} />
-      )}
-    </FlexBetween>
-  );
+			{/* Table View */}
+			<Box>
+				<Link to='/table-view'>
+					<TableChartIcon
+						sx={{
+							fontSize: '24px',
+							color: palette.primary[500],
+							verticalAlign: 'text-bottom',
+							'&:hover': { color: palette.primary[100] },
+						}}
+					/>
+				</Link>
+			</Box>
+			{/* Render the Filter component as a pop-up */}
+			{isFilterOpen && (
+				<Filter isOpen={isFilterOpen} onClose={handleCloseFilter} />
+			)}
+		</FlexBetween>
+	);
 };
 
 export default NavBar;
